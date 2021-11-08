0e04db2d5a2c833d7fabafdb8bdbb59657654949
unknown <vishwarajaug04@gmail.com>
2021-11-08 14:57:12 +0530

    1st commit

--- masai/unit-3/week_6/11-08-2021/assignment/1/js/menu.js
+++ masai/unit-3/week_6/11-08-2021/assignment/1/js/menu.js
@@ -1,1 +1,6 @@
-// www.themealdb.com/api/json/v1/1/categories.php
+// 
+console.log('hi')
+let menu_list = fetch("https:www.themealdb.com/api/json/v1/1/search.php?f=a")
+
+menu_list.then(res=>res.json())
+console.log(res.json)
